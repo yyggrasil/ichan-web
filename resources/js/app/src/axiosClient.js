7bab8b8f39859bf895fa0391f69eb20cc1cefc51
kaue leivas <kaueleivas0@gmail.com>
2025-04-07 16:57:47 -0300

    aula 07/04

---
+++ resources/js/app/src/axiosClient.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+  baseURL: 'http://localhost:8000/api',
+});
+
+export default axiosClient;
+
+
--- resources/js/app/src/views/user/UserFormList.jsx
+++ resources/js/app/src/views/user/UserFormList.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosClient from '../../axiosClient';
+import { data, Link } from 'react-router-dom';
 
 function UserFormList() {
+
+  const [users, setUsers] = React.useState([]);
+
+  const getUsers = () => {
+    axiosClient.get('/user/index')
+              .then(({data}) => {
+                setUsers(data.data);
+              }
+              ).catch((error) => {
+                console.log(error);
+              })
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+
   return (
-    <div>UserFormList</div>
+    <div className='display'>
+      <div className='card animated fadeInDown'>
+        
+        <div style={{
+          display: 'flex',
+          justifyContent: 'justify-content',
+          alignItems: 'center', 
+        }}>
+
+          <h1>Lista de Usuários</h1>
+          <Link to="/user/store" className='btn-add'>Store</Link>
+        </div>
+        <table>
+          
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Email</th>
+              <th className='center actions' colSpan={3}>Ações</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+              users.length > 0 ? (
+                users.map((user, index) => (
+                  <tr key={index}>
+
+                    <td>{user.id}</td>
+
+                    <td>{user.name}</td>
+
+                    <td>{user.email}</td>
+
+                    <td className='center actions'>
+                      <Link to={'/user/update'} className='btn-edit'>Update</Link>
+                    </td>
+
+                    <td className='center actions'>
+                      <Link to={'/user/destroy'} className='btn-delete'>Destroy</Link>
+                    </td>
+                  
+                    <td className='center actions'>
+                      <Link to={'/user/show/'} className='btn-show'>Show</Link>
+                    </td>
+
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td>Nenhum Registro encontrado</td>
+                </tr>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
